f7e6b8abf5484554f248403fcf7824fc581951a9
hzx0901 <1225853481@qq.com>
2023-05-19 21:02:55 +0800

    配置环境

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import installElementPlus from './plugins/element'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.use(store);
+app.use(router);
+installElementPlus(app);
+app.mount("#app");
