e5861b3bc17abfe43d3231986f948b106c87a9b0
hzx0901 <1225853481@qq.com>
2023-06-01 19:05:53 +0800

    fix:修改了logo不能显示的问题

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,15 @@ module.exports = defineConfig({
           },
         },
       },
+      pwa: {
+        iconPaths: {
+            favicon32: 'favicon.ico',
+            favicon16: 'favicon.ico',
+            appleTouchIcon: 'favicon.ico',
+            maskIcon: 'favicon.ico',
+            msTileImage: 'favicon.ico'
+        }
+    },
   transpileDependencies: true,
   lintOnSave: false,
 });
